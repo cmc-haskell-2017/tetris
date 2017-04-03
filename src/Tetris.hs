7413ca9cb7268997b88f7aa64a76a85d8d219eb9
Ilpoln Ilpols <ilya-polozov@mail.ru>
2017-04-03 21:29:56 +0300

    Changing some functions and adding comments

--- src/Tetris.hs
+++ src/Tetris.hs
@@ -1,26 +1,30 @@
-module Tetris where
---Test
--- =========================================
--- Types
--- =========================================
-
-data Shape = J | L | I | S | Z | O | T
-        	deriving (Eq, Show, Enum)
-
 data Block = Free | Full
 
+--Строки нашей доски
 type Row = [Block]
 
+--Все поле
 type Board = [Row]
 
+--Счет
 type Score = Integer
 
-type Coord = (Int, Int,Int)
+--Координаты фигуры: первые две определяют положение фигуру
+--на поле, а третья определяет ее поворот
+type Coord = (Int, Int, Int)
 
+--Состояние игры в текущий момент(разделили доску и фигуру,
+--чтобы при полете фигуры не мигала вся доска, также, чтобы было более 
+--оптимизировано)
 type Game_state = (Board,  Figure)
 
+--Скорость
 type Speed = Float
 
+--Для каждой фигуры свой тип, чтобы однозначно можно было 
+--определить ее и тип операций над ней, например, фигуру I можно вращать
+--произвольно только на расстоянии больше 4 клеток от края,
+--а фигуру O на расстоянии больше 2 клеток от края
 data Figure = O [Coord] |
 			  I [Coord] | 
 			  T [Coord] |
@@ -33,38 +37,55 @@ data Figure = O [Coord] |
 -- Generating
 -- =========================================
 
-gen_figure::Int -> Figure
+--На вход принимается случайное число от 0 до 6, которое определяет
+--Фигуру
+genFigure::Int -> Figure
 
-gen_empty_board::Board
+--Заполняем доску пустыми значениями
+genEmptyBoard::Board
 
-generate_random_figure_list:: StdGen -> [Figure]
+--Генерируем бесконечный список из случайных фигур
+generateRandomFigureList:: StdGen -> [Figure]
 
 
 -- =========================================
 -- Moves
 -- =========================================
 
-
+--Поворачивает фигуру: положение фигуры в пространстве опредляется 
+--двумя числами, функция смотрит, какая ей дана фигура, и вычисляет 
+--расстояние до края доски и на основании этой информации поворачивает ее
+--(если это можно сделать), т.е. изменяет 3 координату
 turn::Figure -> Figure
 
-start_game::Board -> Score
+--Принимает пустую доску, моделирует всю игру, после
+--окончания возвращает счет
+startGame::Board -> Score
 	
-move_left::Figure -> Figure
+--Переещает фигуру влево	
+moveLeft::Figure -> Figure
 
-move_right::Figure -> Figure
+--Перемещает фигуру вправо
+moveRight::Figure -> Figure
 
+--При нажатии клавиши "вниз" роняет фигуру 
 drop::Game_state -> Game_state
 
 -- =========================================
 -- Checking rows and deleting
 -- =========================================
 
-check_rows_to_delete::Board -> [Bool]
+--Смотрит, нет ли строк, которые можно удалить
+checkRowsToDelete::Board -> [Bool]
 
-check_row::Row -> Bool
+--Смотрит, можно ли удаоить строку
+checkRow::Row -> Bool
 
-delete_row::Int -> Board -> Board
+--Удаляет строку
+deleteRow::Int -> Board -> Board
 
+--проверяет, конец игру т.е. приземлилась ли фигура до появления на
+--экране, т.е. конец игры
 gameover :: Game_state -> Bool
 
 
@@ -72,44 +93,52 @@ gameover :: Game_state -> Bool
 -- Drawing
 -- =========================================
 
-draw_Board::Board  -> Picture
+--Рисует доску
+drawBoard::Board  -> Picture
 
-draw_Figure::Figure  ->  Picture
+--Рисует фигуру
+drawFigure::Figure  ->  Picture
 
-draw_tetris ::Game_state-> Picture
+--Рисует тетрис
+drawTetris ::Game_state-> Picture
 
 
 -- =========================================
 -- Updating
 -- =========================================
 
-collides_floor::Game_state -> Bool
+--Проверяет, достигла ли нижняя часть фигуры нижней 
+--границы доски или другой фигуры
+collidesFloor::Game_state -> Bool
 
-collides_side::Game_state -> Bool
+--Проверяет, не выходит ли правая или левая часть фигуры за правую или
+-- левую часть доски соответственно
+collidesSide::Game_state -> Bool
 
-update_board::Figure -> Board ->Board
+--Делает пустые блоки доски, на в которых находится фигура заполненными
+updateBoard::Figure -> Board ->Board
 
-update_speed::Time -> Speed -> Speed
+--На основании прошедшего времени меняет скорость полета фигур
+updateSpeed::Time -> Speed -> Speed
 
-update_tetris :: Float -> Board -> Board
+--Аргумент функции play, обновляет состояние тетриса
+updateTetris :: Float -> Board -> Board
 
 
 -- ===========================================
 -- timing
 -- =======================================
 
-new_tact::Figure -> Board -> Speed -> Game_state
+--Обновляет общее состояние тетриса
+newTact::Figure -> Board -> Speed -> Game_state
 
-new_move::Board -> Game_state
-
-
-
-handle_tetris :: Event -> Game_state -> Game_state
-handle_tetris (Eventkey (SpecialKey KeyRight) Down _ _) = …
-handle_tetris (Eventkey (SpecialKey KeyLeft) Down _ _)  = …
-handle_tetris(Eventkey (SpecialKey KeyDown) Down _ _ ) = …
-handle_tetris (Evenkey (SpecialKey KeyUp) Down _ _ ) = …
+--Застявляет фигуру постоянно падать, вызываем эту фунцию на каждом такте
+newMove::Board -> Game_state
 
 
-
-
+--Аргумент функции play, которя говорит, что длает каждая клавиша
+handleTetris :: Event -> Game_state -> Game_state
+handleTetris (Eventkey (SpecialKey KeyRight) Down _ _) = …
+handleTetris (Eventkey (SpecialKey KeyLeft) Down _ _)  = …
+handleTetris(Eventkey (SpecialKey KeyDown) Down _ _ ) = …
+handleTetris (Evenkey (SpecialKey KeyUp) Down _ _ ) = …
