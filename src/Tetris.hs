a371bf9a0e42f14689d2d80c352a1576cdcc017c
DeadBread <zhikov_n1@mail.ru>
2017-04-12 16:57:42 +0300

    new representation of figure. Added turning function

--- src/Tetris.hs
+++ src/Tetris.hs
@@ -6,14 +6,8 @@ import Graphics.Gloss.Geometry.Line
 import Graphics.Gloss.Interface.Pure.Game
 
 run :: IO ()
-run = do
-  --putStrLn "This project is not yet implemented"
- 
-   play display bgColor fps (genEmptyBoard ) drawTetris handleTetris updateTetris
-   where
-    display = InWindow "Tetris" (screenWidth, screenHeight) (200, 200)
-    bgColor = black   -- цвет фона
-    fps     = 200    -- кол-во кадров в секунду
+run = putStrLn "This project is not yet implemented"
+
 
 -- =========================================
 -- Types
@@ -24,7 +18,7 @@ run = do
 
 
                                --data Shape = J | L | I | S | Z | O | T
-                               --         deriving (Eq, Show, Enum)
+                               --       	deriving (Eq, Show, Enum)
 --Клетка заполнена?
 data Block = Free | Full
 
@@ -41,19 +35,12 @@ type Score = Integer
 --их последовательностью
 type Coord = (Int, Int)
 
-type Time = Float
-
 --Состояние игры в текущий момент(разделили доску и фигуру,
 --чтобы при полете фигуры не мигала вся доска, также, чтобы было более 
 --оптимизировано)
---type Gamestate = (Board,  Figure, Speed, Time)
+type Time = Float
+type Gamestate = (Board,  Figure, Speed, Time)
 
-data Gamestate = Gamestate
-  { board   :: Board  
-  , figure  :: Figure
-  , speed   :: Speed
-  , time    :: Time    
-  }
 --Скорость
 type Speed = Float
 
@@ -61,13 +48,15 @@ type Speed = Float
 --определить ее и тип операций над ней, например, фигуру I можно вращать
 --произвольно только на расстоянии больше 4 клеток от края,
 --а фигуру O на расстоянии больше 2 клеток от края
-data Figure = O [Coord] |
-        I [Coord] | 
-        T [Coord] |
-        J [Coord] | L  [Coord] | 
-        S [Coord] | Z  [Coord] 
-          deriving(Eq, Show)
-  
+
+data FigureType = O | I | T | J | L | S | Z
+											deriving(Eq, Show)
+
+data Direction = DUp | DDown | DLeft | DRight
+											deriving(Eq, Show)
+
+data Figure = Figure FigureType Direction Coord 
+											deriving(Eq, Show)
 
 
 -- | Ширина экрана.
@@ -89,23 +78,14 @@ screenHeight = 450
 --На вход принимается случайное число от 0 до 6, которое определяет
 --Фигуру
 genFigure::Int -> Figure
-genFigure _ = O[(0,0),(0,0)]
+genFigure _ = Figure O DUp (0,0)
 
 --Заполняем доску пустыми значениями
-
-genEmptyBoard :: Gamestate
-genEmptyBoard = Gamestate
-  { board = [[Free]]
-  , figure  = O[(0,0)]
-  , speed  = 0
-  , time   = 0
-  }
-
-
---           ([[Free]],O[(0,0),(0,0)],0,0)
+genEmptyBoard::Board
+genEmptyBoard =  [[Free]]
 --Генерируем бесконечный список из случайных фигур
 generateRandomFigureList:: StdGen -> [Figure]
-generateRandomFigureList _ =  [O[(0,0),(0,0)]]
+generateRandomFigureList _ =  [Figure O DUp (0,0)]
 
 
 
@@ -120,26 +100,85 @@ generateRandomFigureList _ =  [O[(0,0),(0,0)]]
 --двумя числами, функция смотрит, какая ей дана фигура, и вычисляет 
 --расстояние до края доски и на основании этой информации поворачивает ее
 --(если это можно сделать), т.е. изменяет 3 координату
+
+-- data Figure = O Direction Coord |
+-- 			  I Direction Coord | 
+-- 			  T Direction Coord |
+-- 			  J Direction Coord | L  Direction Coord | 
+-- 			  S Direction Coord | Z  Direction Coord
+-- 			  	deriving(Eq, Show)
+
+-- data Direction = Up | Down | Left | Right
+type BlockedFigure = (Coord, Coord, Coord, Coord)
+
+
 turn::Figure -> Figure
-turn _ =  O[(0,0),(0,0)]
+turn (Figure t DUp c) = Figure t DRight c
+turn (Figure t DRight c) = Figure t DDown c
+turn (Figure t DDown c) = Figure t DLeft c
+turn (Figure t DLeft c) = Figure t DUp c
+
+
+
+figureToDraw::Figure->BlockedFigure
+figureToDraw (Figure O d c) = figureToDrawO (Figure O d c)
+figureToDraw (Figure I d c) = figureToDrawI (Figure I d c)
+figureToDraw (Figure T d c) = figureToDrawT (Figure T d c)
+figureToDraw (Figure J d c) = figureToDrawJ (Figure J d c)
+figureToDraw (Figure L d c) = figureToDrawL (Figure L d c)
+figureToDraw (Figure S d c) = figureToDrawS (Figure S d c)
+figureToDraw (Figure Z d c) = figureToDrawZ (Figure Z d c)
+
+
+figureToDrawO::Figure -> BlockedFigure
+figureToDrawO (Figure O _ (x, y)) = ((x, y), (x+1, y), (x, y-1), (x+1, y-1))
+
+
+figureToDrawI::Figure -> BlockedFigure
+figureToDrawI (Figure I d (x, y)) | (d == DUp) || (d == DDown) = ((x, y+1), (x, y), (x, y-1), (x, y-2))
+				 				  | otherwise = ((x-1, y), (x, y), (x+1, y), (x+2, y))
+
+figureToDrawZ::Figure -> BlockedFigure
+figureToDrawZ (Figure Z d (x, y)) | (d == DUp) || (d == DDown) = ((x-1, y+1), (x-1, y), (x, y), (x, y-1))
+				  				  | otherwise = ((x-1, y), (x, y), (x, y+1), (x+1, y+1))
+
+figureToDrawS::Figure -> BlockedFigure
+figureToDrawS (Figure S d (x, y)) | (d == DUp) || (d == DDown) = ((x, y-1), (x, y), (x+1, y), (x+1, y+1))
+				  				  | otherwise = ((x-1, y), (x, y), (x, y-1), (x+1, y-1))
+
+
+figureToDrawJ::Figure -> BlockedFigure
+figureToDrawJ (Figure J d (x,y)) | d == DDown = ((x, y+1), (x, y), (x, y-1), (x-1, y-1))
+								 | d == DUp = ((x, y-1), (x, y), (x, y+1), (x+1, y+1))
+								 | d == DRight = ((x-1, y), (x, y), (x+1, y), (x+1, y-1))
+								 | otherwise = ((x-1, y+1), (x-1, y), (x, y), (x+1, y))
+
+
+figureToDrawL::Figure -> BlockedFigure
+figureToDrawL (Figure L d (x,y)) | d == DDown = ((x, y+1), (x, y), (x, y-1), (x+1, y-1))
+								 | d == DUp = ((x, y-1), (x, y), (x, y+1), (x-1, y+1))
+								 | d == DRight = ((x-1, y), (x, y), (x+1, y), (x+1, y+1))
+								 | otherwise = ((x-1, y-1), (x-1, y), (x, y), (x+1, y))
+
+figureToDrawT::Figure -> BlockedFigure
+figureToDrawT (Figure T d (x,y)) | d == DDown = ((x-1, y), (x, y), (x+1, y), (x, y-1))
+								 | d == DUp = ((x-1, y), (x, y), (x+1, y), (x, y+1))
+								 | d == DRight = ((x, y+1), (x, y), (x, y-1), (x+1, y))
+								 | otherwise = ((x, y+1), (x, y), (x, y-1), (x-1, y))
+
 --Принимает пустую доску, моделирует всю игру, после
 --окончания возвращает счет
 startGame::Board -> Score
 startGame  _ =  0
---Переещает фигуру влево  
+--Переещает фигуру влево	
 moveLeft::Figure -> Figure
-moveLeft _ =  O[(0,0),(0,0)]
+moveLeft _ =  Figure O DUp (0,0)
 --Перемещает фигуру вправо
 moveRight::Figure -> Figure
-moveRight _ =  O[(0,0),(0,0)]
+moveRight _ =  Figure O DUp (0,0)
 --При нажатии клавиши "вниз" роняет фигуру 
 drop::Gamestate -> Gamestate
-drop  _ =  Gamestate
-  { board = [[Free]]
-  , figure  = O[(0,0)]
-  , speed  = 0
-  , time   = 0
-  }
+drop  _ =  ([[Free]],Figure O DUp (0,0),0,0)
 
 
 
@@ -216,15 +255,9 @@ drawBlock _ =  translate (-w) h (scale 30 30 (pictures
     h = fromIntegral screenHeight / 2
 
 
-
-
-
 -- =========================================
 -- Updating
 -- =========================================
-
-
-
 
 
 --Проверяет, достигла ли нижняя часть фигуры нижней 
@@ -245,22 +278,12 @@ updateBoard _ _ =  [[Free]]
 updateSpeed::Time -> Speed -> Speed
 updateSpeed _ _ = 0
 --Аргумент функции play, обновляет состояние тетриса
-updateTetris :: Float -> Gamestate -> Gamestate
-updateTetris _ _ =  Gamestate
-  { board = [[Free]]
-  , figure  = O[(0,0)]
-  , speed  = 0
-  , time   = 0
-  }
+updateTetris :: Float -> Board -> Board
+updateTetris _ _ =  [[Free]]
 
 --Обновить весь тетрис
 updateTheWholeTetris:: Time -> Speed -> Gamestate -> Gamestate
-updateTheWholeTetris _ _ _ =  Gamestate
-  { board = [[Free]]
-  , figure  = O[(0,0)]
-  , speed  = 0
-  , time   = 0
-  }
+updateTheWholeTetris _ _ _ =  ([[Free]],Figure O DUp (0,0),0,0)
 -- ===========================================
 -- timing
 -- =======================================
@@ -271,37 +294,16 @@ updateTheWholeTetris _ _ _ =  Gamestate
 
 --Обновляет общее состояние тетриса
 newTact::Figure -> Board -> Speed -> Gamestate
-newTact _ _ _ =  Gamestate
-  { board = [[Free]]
-  , figure  = O[(0,0)]
-  , speed  = 0
-  , time   = 0
-  }
+newTact _ _ _ =  ([[Free]],Figure O DUp (0,0),0,0)
 
 --Застявляет фигуру постоянно падать, вызываем эту фунцию на каждом такте
 newMove::Board -> Gamestate
-newMove _ =  Gamestate
-  { board = [[Free]]
-  , figure  = O[(0,0)]
-  , speed  = 0
-  , time   = 0
-  }
+newMove _ =  ([[Free]],Figure O DUp (0,0),0,0)
 
 
 --Аргумент функции play, которя говорит, что длает каждая клавиша
 handleTetris :: Event -> Gamestate -> Gamestate
-handleTetris (EventKey (SpecialKey KeyRight) Down _ _)  = bumpPlayer
-handleTetris (EventKey (SpecialKey KeyLeft) Down _ _)   = bumpPlayer
-handleTetris(EventKey (SpecialKey KeyDown) Down _ _ )  = bumpPlayer
-handleTetris (EventKey (SpecialKey KeyUp) Down _ _ )  = bumpPlayer
-
-bumpPlayer :: Gamestate -> Gamestate
-bumpPlayer _ = Gamestate
-  { board = [[Free]]
-  , figure  = O[(0,0)]
-  , speed  = 0
-  , time   = 0
-  }
- 
-
-
+handleTetris (EventKey (SpecialKey KeyRight) Down _ _) _ = ([[Free]],Figure O DUp (0,0),0,0)
+handleTetris (EventKey (SpecialKey KeyLeft) Down _ _)  _ = ([[Free]],Figure O DUp (0,0),0,0)
+handleTetris(EventKey (SpecialKey KeyDown) Down _ _ ) _ = ([[Free]],Figure O DUp (0,0),0,0)
+handleTetris (EventKey (SpecialKey KeyUp) Down _ _ ) _ = ([[Free]],Figure O DUp (0,0),0,0)
